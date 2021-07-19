4029cc3df21d7f073e7a9ab96e5a0758448ccde3
Junandia <email@junandia.id>
2021-07-19 12:04:04 +0700

    add index.php at link thme

--- application/views/tema.php
+++ application/views/tema.php
@@ -12,14 +12,14 @@
     <title>Azzainiyyah - MANKEU</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?php echo base_url('index.php/assets/theme/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/theme/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?php echo base_url('index.php/assets/theme/') ?>css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="<?php echo base_url('index.php/assets/theme/') ?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/theme/') ?>css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/theme/') ?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
 
@@ -440,27 +440,27 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?php echo base_url('index.php/assets/theme/') ?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?php echo base_url('index.php/assets/theme/') ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url('assets/theme/') ?>vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url('assets/theme/') ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url('index.php/assets/theme/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= base_url('assets/theme/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?php echo base_url('index.php/assets/theme/') ?>js/sb-admin-2.min.js"></script>
+    <script src="<?php echo base_url('assets/theme/') ?>js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="<?php echo base_url('index.php/assets/theme/') ?>vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?php echo base_url('index.php/assets/theme/') ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="<?php echo base_url('assets/theme/') ?>vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url('assets/theme/') ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="<?php echo base_url('index.php/assets/theme/') ?>js/demo/datatables-demo.js"></script>
+    <script src="<?php echo base_url('assets/theme/') ?>js/demo/datatables-demo.js"></script>
 
     <!-- SweetAlert -->
-    <script src="<?= base_url('index.php/assets/sweetalert/')?>sweetalert2.all.min.js"></script>
+    <script src="<?= base_url('assets/sweetalert/')?>sweetalert2.all.min.js"></script>
 
     <!-- SweetAlert Customized -->
-    <script src="<?= base_url('index.php/assets/sweetalert/')?>customizedSA.js"></script>
+    <script src="<?= base_url('assets/sweetalert/')?>customizedSA.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 
